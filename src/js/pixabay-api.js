9436bd07ba2d9506698c0f79327d2732d7b5c88f
Vladyslav Voloshyn <v.vlados211@gmail.com>
2026-09-05 19:38:37 +0200

    update file

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -4,13 +4,15 @@ const API_KEY = '57439014-651b11918c0474541601eccf4';
 const BASE_URL = 'https://pixabay.com/api/';
 
 export function getImagesByQuery(query) {
-  return axios.get(BASE_URL, {
-    params: {
-      key: API_KEY,
-      q: query,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: true,
-    },
-  });
+  return axios
+    .get(BASE_URL, {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+      },
+    })
+    .then(response => response.data);
 }
